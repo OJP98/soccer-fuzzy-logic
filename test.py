54e7c40ee2ffe9d90db0c40d0c1a5d96061fec05
josepablito16 <josepablocif16@gmail.com>
2020-10-26 23:57:19 -0600

    Se agregan funciones de pertenencia para la distancia

--- test.py
+++ test.py
@@ -13,4 +13,14 @@ puntos = [[0, 0.25], [2, 0.25], [4, 0.32], [6, 0.32], [8, 0.6], [10, 0.75]]
 assert DistanciaPelotaJugador(
     jugadorX, jugadorY, pelotaX, pelotaY) == 5.0, "Should be 5.0"
 assert CalcularCentroide(puntos) == 6.425702811244979, "Should be 6.42"
+
+
+assert lejos(7) == 0.3333333333333333, "Should be 0.33"
+
+assert lejos(0) == 0, "Should be 0"
+assert lejos(5) == 1, "Should be 1"
+
+assert cerca(0) > 0.9, "Should be > .9"
+assert cerca(10) < 0.01, "Should be <0.01"
+
 print("Todo bien crack!")
