d700a7f787e51cb16122dc1a2a1cdef2bd416664
OJP98 <ojuarez_p@hotmail.com>
2020-10-27 16:20:39 -0600

    Player can move when clicked
    - Added assets for player and field
    - Added player class itself

--- soccer/src/components/App.jsx
+++ soccer/src/components/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Button } from '@material-ui/core'
 import Player from './Player/Player.jsx'
+import Field from './Field/Field.jsx'
 import './style.css'
 
 export default class App extends React.Component {
@@ -14,7 +15,9 @@ export default class App extends React.Component {
     return (
       <div className="title">
         <h1>Soccer with Fuzzy Logic</h1>
-        <Player x="10" y="10" />
+        <Field>
+          <Player x="200" y="100" />
+        </Field>
         <Button color="primary">This is a MUI button</Button>
       </div>
     )
